4ebdfffa44c1c29e5a6769bd35767e361a188334
repl.it user <replituser@example.com>
2021-03-01 05:12:27 +0000

    not sorted

--- c06/c03/ft_sort_params.c
+++ c06/c03/ft_sort_params.c
@@ -14,12 +14,21 @@ int     main(int argc, char **argv)
 		{
 			if (argv[i] < argv[j])
 			{
-				temp = argv[i];
+				*temp = *argv[i];
 				*argv[i] = *argv[j];
-				*argv[j] = temp;
+				*argv[j] = *temp;
 			}
 			j++;
 		}
+		i++;
+	}
+	i = 1;
+	while (i < argc)
+	{
+		j = -1;
+		while (argv[i][++j])
+			write(1, &argv[i][j], 1);
+		write(1, "\n", 1);
 		i++;
 	}
 	return (0);
