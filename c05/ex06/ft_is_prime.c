d14c62fb41aa32a8e915c81f5a452b45636954ae
repl.it user <replituser@example.com>
2021-03-01 04:47:51 +0000

    c05 complete

--- c05/ex06/ft_is_prime.c
+++ c05/ex06/ft_is_prime.c
@@ -5,7 +5,7 @@ int	ft_is_prime(int nb)
 	int i;
 
 	i = 2;
-	while (i < nb)
+	while (i * i < nb)
 	{
 		if (nb % i == 0)
 			return 0;
--- c05/ex07/ft_find_next_prime.c
+++ c05/ex07/ft_find_next_prime.c
@@ -22,7 +22,7 @@ int	is_prime(int nb)
 	int i;
 
 	i = 2;
-	while (i < nb / 2)
+	while (i * i < nb)
 	{
 		if (nb % i == 0)
 			return 0;
