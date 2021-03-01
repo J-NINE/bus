19404644b35f9ed7c3fff2fa8c1bd43274732811
repl.it user <replituser@example.com>
2021-03-01 05:03:42 +0000

    c06 ex02

--- c06/ex02/ft_rev_params.c
+++ c06/ex02/ft_rev_params.c
@@ -6,12 +6,13 @@ int     main(int argc, char **argv)
         int j;
 
         i = argc - 1;
-        while (i <= 0)
+        while (i > 0)
 	{
 		j = -1;
 		while (argv[i][++j])
 			write(1, &argv[i][j], 1);
 		write(1, "\n", 1);
+		i--;
 	}
 	return (0);
 }
