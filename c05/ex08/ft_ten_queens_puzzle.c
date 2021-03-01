456bf48040802c2c56712c1ce18b92c4905e339a
repl.it user <replituser@example.com>
2021-03-01 03:34:38 +0000

    c05 ex08

--- c05/ex08/ft_ten_queens_puzzle.c
+++ c05/ex08/ft_ten_queens_puzzle.c
@@ -1,13 +1,64 @@
-#include <stdio.h>
 #include <unistd.h>
 
-int	ft_ten_queens_puzzle(void)
+void	ft_putchar(char c)
 {
+	write(1, &c, 1);
+}
 
+int	is_possible_place(int table[10], int row, int col)
+{
+	int i;
+
+	i = -1;
+	while (++i < row)
+		if (col == table[i] ||
+				i + table[i] == row + col ||
+				i - table[i] == row - col)
+			return (0);
+	return (1);
+}
+
+void	ft_ten_queens_puzzle_recur(int table[10], int *res, int pos)
+{
+	int i;
+	int i2;
+
+	if (pos == 10)
+	{
+		*res += 1;
+		i2 = -1;
+		while (++i2 < 10)
+			ft_putchar(table[i2] + '0');
+		ft_putchar('\n');
+	}
+	else
+	{
+		i = -1;
+		while (++i < 10)
+			if (is_possible_place(table, pos, i))
+			{
+				table[pos] = i;
+				ft_ten_queens_puzzle_recur(table, res, pos + 1);
+			}
+	}
+}
+
+int		ft_ten_queens_puzzle(void)
+{
+	int table[10];
+	int i;
+	int res;
+
+	i = -1;
+	while (++i < 10)
+		table[i] = -1;
+	res = 0;
+	ft_ten_queens_puzzle_recur(table, &res, 0);
+	return (res);
 }
 
 int	main(void)
 {
-	printf("\n%d", ft_ten_queens_puzzle());
+	ft_ten_queens_puzzle();
 	return 0;
 }
