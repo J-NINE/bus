7e0fefb7970af9a42a423de887e6026ee91755ef
repl.it user <replituser@example.com>
2021-03-01 05:02:11 +0000

    test

---
+++ c06/ex02/ft_rev_params.c
@@ -0,0 +1,17 @@
+#include <unistd.h>
+
+int     main(int argc, char **argv)
+{
+        int i;
+        int j;
+
+        i = argc - 1;
+        while (i <= 0)
+	{
+		j = -1;
+		while (argv[i][++j])
+			write(1, &argv[i][j], 1);
+		write(1, "\n", 1);
+	}
+	return (0);
+}
