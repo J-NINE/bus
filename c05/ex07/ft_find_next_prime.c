8a23001768e4d418326868a8309e2b5468232208
repl.it user <replituser@example.com>
2021-03-01 04:43:08 +0000

    comment

--- c05/ex07/ft_find_next_prime.c
+++ c05/ex07/ft_find_next_prime.c
@@ -1,11 +1,28 @@
 #include <stdio.h>
 
+/*
+bool arr[];
+arr[0] = true;
+arr[1] = true;
+for(int i = 2; i<nb/2; i++)
+{
+	if(arr[i])	return;
+	for(int j = 2 * i; j < mb; j += i)
+	{
+		arr[j] = true;
+	}
+}
+
+arr[nb/2] return 소수;
+
+*/
+
 int	is_prime(int nb)
 {
 	int i;
 
 	i = 2;
-	while (i < nb)
+	while (i < nb / 2)
 	{
 		if (nb % i == 0)
 			return 0;
@@ -30,6 +47,6 @@ int	ft_find_next_prime(int nb)
 
 int	main(void)
 {
-	printf("%d ", ft_find_next_prime(6));
+	printf("%d ", ft_find_next_prime(462832632));
 	return 0;
 }
