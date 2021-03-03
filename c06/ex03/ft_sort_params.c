5ea82dbef37e4fcde5e5cc8cb8f59b0a50ed6d60
repl.it user <replituser@example.com>
2021-03-03 00:23:26 +0000

    c05 ex03

--- c06/ex03/ft_sort_params.c
+++ c06/ex03/ft_sort_params.c
@@ -1,35 +1,55 @@
 #include <unistd.h>
 
+int	ft_strcmp(char *s1, char *s2)
+{
+	int i;
+	int length_1;
+	int length_2;
+
+	length_1 = -1;
+	while (s1[++length_1]);
+	length_2 = -1;
+	while (s2[length_2]);
+	if (length_1 > length_2)
+		return (1);
+	else if(length_1 < length_2)
+		return (-1);
+	i = -1;
+	while (s1[++i])
+		if (s1[i] != s2[i])
+			return (s1[i] > s2[i]);
+	return (0);
+}
+
 int     main(int argc, char **argv)
 {
-        int i;
-        int j;
+
+	int i;
+	int j;
 	char *temp;
 
-        i = 1;
-        while (i <= 0)
+	i = 0;
+	while (++i < argc - 1)
 	{
-		j = i+1;
-		while (argv[j])
+		j = i;
+		while (++j < argc)
 		{
-			if (argv[i] < argv[j])
+			if (ft_strcmp(argv[i], argv[j]) > 0)
 			{
-				*temp = *argv[i];
-				*argv[i] = *argv[j];
-				*argv[j] = *temp;
+				temp = argv[i];
+				argv[i] = argv[j];
+				argv[j] = temp;
 			}
-			j++;
 		}
-		i++;
 	}
-	i = 1;
-	while (i < argc)
+
+	i = 0;
+	while (++i < argc)
 	{
 		j = -1;
 		while (argv[i][++j])
 			write(1, &argv[i][j], 1);
 		write(1, "\n", 1);
-		i++;
 	}
 	return (0);
 }
