3d793e3d353cfdc9418156c648b9b9b4a884093e
repl.it user <replituser@example.com>
2021-03-03 00:52:56 +0000

    c07 ex02 frame only

---
+++ c07/ex02/ft_ultimate_range.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int	ft_ultimate_range(int **range, int min, int max)
+{
+	if(min > max)
+		return 0;
+}
+
+int	main(void)
+{
+	int **range;
+	return 0;
+}
+
