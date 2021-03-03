54d7d0e313a0c185288d8fa06ca2eb235590d1cc
repl.it user <replituser@example.com>
2021-03-03 00:42:47 +0000

    c07 ex01

---
+++ c07/ex01/ft_range.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int	*ft_range(int min, int max)
+{
+	int i;
+	int *result;
+
+	result = malloc((int)sizeof(int) * (max - min));
+	i = -1;
+	while (min < max)
+		result[++i] = min++;
+	return (result);
+}
+
+int	main(void)
+{
+	int i;
+	int *result;
+	
+	result = ft_range(0, 4);
+	i = 0;
+	while (i < 4)
+	{
+		printf("%d", result[i]);
+		i++;
+	}
+	return (0);
+}
