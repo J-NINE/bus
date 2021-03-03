0721d1f2032f369a18d4d8e68656459f817cf677
repl.it user <replituser@example.com>
2021-03-03 01:21:56 +0000

    c07 ex02 dump

--- c07/ex02/ft_ultimate_range.c
+++ c07/ex02/ft_ultimate_range.c
@@ -3,13 +3,31 @@
 
 int	ft_ultimate_range(int **range, int min, int max)
 {
-	if(min > max)
-		return 0;
+	int i;
+	if (min >= max)
+	{
+		range = NULL;
+		return (0);
+	}
+	range = malloc((int)sizeof(int*) * (max - min));
+	
+	i = 0;
+	while (min < max)
+		range[i++][0] == min++;
+	return (max - min);
 }
 
 int	main(void)
 {
 	int **range;
+	int len = ft_ultimate_range(range, 0, 5);
+	int i = -1;
+	
+	printf("!");
+	while (range[++i])
+	{
+		printf("%d", range[i][0]);
+	}
 	return 0;
 }
 
