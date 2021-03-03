763052318ed63b16028232ec749f2b21497a85a0
repl.it user <replituser@example.com>
2021-03-03 02:59:51 +0000

    c07 ex04 isvalid

--- c07/ex04/ft_convert_base.c
+++ c07/ex04/ft_convert_base.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
 int	ft_strlen(char* str)
 {
 	int i;
@@ -10,6 +9,30 @@ int	ft_strlen(char* str)
 	while (str[++i]);
 	return (i);
 }
+
+int	is_valid(char *base)
+{
+	int i;
+	int j;
+
+	i = -1;
+	while (base[i++])
+	{
+		if (base[i] == '+' || base[i] == '-' || base[i] == ' ')
+			return 0;
+		j = i + 1;
+		while (base[j])
+		{
+			if(base[i] == base[j])
+				return 0;
+			j++;
+		}
+	}
+	if (i <= 1)
+		return 0;
+	return 1;
+}
+
 char	*ft_convert_base(char *nbr, char *base_from, char *base_to)
 {
 	if(ft_strlen(base_from) != ft_strlen(base_to))
