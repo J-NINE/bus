8ce1b0908e33dadd64b640e3bebd40ae8d39c537
Gooah Jung <gojung@c8r5s4.42seoul.kr>
2021-03-09 02:28:56 +0900

    C01

---
+++ C02/ft_putstr_non_printable.c
@@ -0,0 +1,52 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putstr_non_printable.c                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gojung <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/03/09 00:53:45 by gojung            #+#    #+#             */
+/*   Updated: 2021/03/09 00:57:41 by gojung           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	print_hex(int np)
+{
+	char	*hex;
+
+	hex = "0123456789abcdef";
+	if (np > 16)
+	{
+		print_hex(np / 10);
+		print_hex(np % 10);
+	}
+	else
+		write(1, &hex[np], 1);
+}
+
+void	ft_putstr_non_printable(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i])
+	{
+		if ((str[i] <= 31 && str[i] >= 0) || str[i] == 127)
+		{
+			write(1, "\\", 1);
+			if (str[i] < 16)
+				write(1, "0", 1);
+			print_hex(str[i]);
+		}
+		else
+			write(1, &str[i], 1);
+		i++;
+	}
+}
+
+int	main(void)
+{
+	ft_putstr_non_printable("Coucou\ntu vas bien ?");
+}
